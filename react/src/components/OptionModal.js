dc18642f9396d804fb38f9dd81b5d3669b125c21
Marie Treschow <marie.treschow@mobgen.com>
2018-06-03 21:06:44 +0200

    adding styling for react modal

--- react/src/components/OptionModal.js
+++ react/src/components/OptionModal.js
@@ -7,11 +7,13 @@ const OptionModal = (props) => (
     isOpen={!!props.selectedOption}
     contentLabel="Selected Option"
     onRequestClose={props.handleClearState}
+    closeTimeoutMS={200}
+    className='modal'
     >
 
-    <h3>Selected Options</h3>
-    {props.selectedOption && <p>{props.selectedOption}</p>}
-    <button onClick={props.handleClearState}>OK</button>
+    <h3 className='modal__title'>Selected Options</h3>
+    {props.selectedOption && <p className='modal__body' >{props.selectedOption}</p>}
+    <button className='button' onClick={props.handleClearState}>OK</button>
   </Modal>
 );
 
